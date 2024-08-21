1c302b8b86f439935a8de5a7930638ba56cd7012
IgboPharaoh <solomoneze62@gmail.com>
2024-08-21 14:22:27 +0100

    build: set config requirements

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,48 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      colors: {
+        gray: {
+          "custom-100": "#FAFAFA",
+          "custom-200": "#858585",
+          "custom-300": "#B5B5B5",
+          "custom-400": "#BFBFBF",
+          "custom-500": "#EBEBEB",
+          "custom-600": "#A3A3A3",
+          "custom-700": "#F0F0F0",
+          "custom-800": "#808080",
+          "custom-900": "#F7F7F7",
+        },
+        blue: {
+          "custom-100": "#2A58FE",
+        },
+        orange: {
+          "custom-100": "#F7931A",
+          "custom-200": "#FFF5EB",
+          "custom-300": "#E0E0E0",
+          "custom-400": "#E68108",
+          "custom-500": "#FEF6EB",
+          "custom-600": "#FADEB7",
+          "custom-700": "#FFF0E0",
+        },
+        purple: {
+          "custom-100": "#F2F0FF",
+          "custom-200": "#7B66FF",
+        },
+        "custom-black": {
+          "custom-100": "#292929",
+        },
+      },
+      fontFamily: {
+        brawler: ["var(--brawler-font)"],
+        inter: ["var(--inter-font)"],
       },
     },
   },
