e540db7b66349f6a9c1afb7ee36f7b5aceee78cd
IgboPharaoh <solomoneze62@gmail.com>
2024-08-21 14:29:19 +0100

    chore: create landing page with responsive styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+export const manrope = Manrope({ subsets: ["latin"], display: "swap" });
 
 export const metadata: Metadata = {
   title: "Bitcoin transcript registry",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={manrope.className}>{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,21 @@
 import React from "react";
 import { allTranscripts } from "contentlayer/generated";
-import Link from "next/link";
 
 import { organizeContent } from "@/utils";
+import { HeroSection, Header, FeaturedTranscripts, ExploreTranscripts, FooterTop, WhyTranscripts, FooterComponent } from "./components/index";
 
 export default function Home() {
   const contentTree = organizeContent(allTranscripts);
 
   return (
     <div>
-      <h1>Bitcoin Transcripts</h1>
-      <ul>
-        {Object.keys(contentTree).map((key) => (
-          <li key={key}>
-            <Link href={`/${key}`}>{key}</Link>
-          </li>
-        ))}
-      </ul>
+      <Header />
+      <HeroSection />
+      <FeaturedTranscripts />
+      <WhyTranscripts />
+      <ExploreTranscripts />
+      <FooterTop />
+      <FooterComponent />
     </div>
   );
 }
