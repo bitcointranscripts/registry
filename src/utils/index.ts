da599bd5c73f156708eef2f9ed6246e3c23bc708
IgboPharaoh <solomoneze62@gmail.com>
2024-09-09 19:30:17 +0100

    enhance contentlayer algorithm

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -41,32 +41,39 @@ export function shuffle(data: Transcript[]) {
 }
 
 export const extractTranscripts = (allTranscripts: Transcript[]) => {
-  const transcripts = allTranscripts.filter((transcript) => !!transcript.date);
-  const date = new Date();
+  const CURRENT_DAY = Date.now();
+  const ONE_DAY = 86_400_000; // 1000 * 3600 * 24
 
-  const CURRENT_DAY = date.getTime();
-  const ONE_DAY = 1000 * 3600 * 24;
+  const languageCodes = ["zh.md", "es.md"];
+  const transcripts = allTranscripts.filter((transcript) => {
+    const fileName = transcript._raw.sourceFileName;
+    return transcript.date && languageCodes.some((code) => fileName.endsWith(code));
+  });
 
-  const latestTranscripts = transcripts
-    .sort((a, b) => new Date(b.date as string).getTime() - new Date(a.date as string).getTime())
-    .slice(0, 3)
-    .map((transcript) => {
-      return {
-        ...transcript,
-        days_opened: Math.floor((CURRENT_DAY - new Date(transcript.date as string).getTime()) / ONE_DAY),
-      };
-    });
+  // Sort and slice in one pass
+  const latestTranscripts = transcripts.reduce((acc, transcript) => {
+    const transcriptDate = new Date(transcript.date as string).getTime();
+    const days_opened = Math.floor((CURRENT_DAY - transcriptDate) / ONE_DAY);
 
-  const featuredTranscripts = getFeaturedTranscripts(transcripts);
+    acc.push({ ...transcript, days_opened });
+    acc.sort((a, b) => new Date(b.date as string).getTime() - new Date(a.date as string).getTime());
+
+    if (acc.length > 3) acc.pop();
+    return acc;
+  }, [] as (Transcript & { days_opened: number })[]);
+
+  const featuredTranscripts = getFeaturedTranscripts(allTranscripts);
 
   return { latestTranscripts, featuredTranscripts };
 };
 
-export const getFeaturedTranscripts = (allTranscripts: Transcript[], callbackFn?: React.Dispatch<React.SetStateAction<Transcript[]>>) => {
-  const featuredTranscripts = shuffle(allTranscripts).filter((transcript) => !!transcript.speakers);
-  callbackFn && callbackFn(featuredTranscripts.slice(0, 3));
+export const getFeaturedTranscripts = (allTranscripts: Transcript[]) => {
+  const featuredTranscripts = allTranscripts
+    .filter((transcript) => transcript.speakers)
+    .sort(() => Math.random() - 0.5)
+    .slice(0, 30);
 
-  return featuredTranscripts.slice(0, 3);
+  return featuredTranscripts;
 };
 
 export function createSlug(name: string): string {
