63e0dbdb13889677fe3fc126c513b269b1d6c387
0tuedon <90271995+0tuedon@users.noreply.github.com>
2024-09-12 12:33:45 +0100

    fix: made scroll behaviour smooth

--- src/app/components/explore/AlphabetGrouping.tsx
+++ src/app/components/explore/AlphabetGrouping.tsx
@@ -5,7 +5,7 @@ import React, { useState } from 'react'
 const AlphabetGrouping = () => {
     const [allCharacters] = useState(alphabeticalArrangement)
   return (
-    <div className='grid grid-cols-5 p-6 gap-6 max-w-[355px] w-full border border-gray-custom-1200 rounded-md'>
+    <div className='fixed top-10 right-20 grid grid-cols-5 p-6 gap-6 max-w-[355px] w-full border border-gray-custom-1200 rounded-md'>
             {allCharacters.map(char => 
 
                 <Link href={`#${char.toLowerCase()}`} className='h-10 w-10 text-lg' >
--- src/app/components/explore/GroupedTranscriptContent.tsx
+++ src/app/components/explore/GroupedTranscriptContent.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import SingleTranscriptContent from './SingleTranscriptContent'
 
-const GroupedTranscriptContent = () => {
+const GroupedTranscriptContent = ({
+  topicsByAlphabet,
+}: IGroupedTranscriptContent) => {
+
   return (
-    <div className='flex flex-col gap-7'>
-        <h4 className='font-bold text-2xl'>A</h4>
-        <div className='grid grid-cols-2 gap-2.5'>
-                <SingleTranscriptContent />
-                <SingleTranscriptContent />
-        </div>
+    <div id={topicsByAlphabet.letter.toLowerCase()} className="flex flex-col gap-7">
+      <h4 className="font-bold text-2xl">{topicsByAlphabet.letter}</h4>
+      <div className="grid grid-cols-2 gap-2.5">
+        {topicsByAlphabet.titles.map((topics) => (
+          <SingleTranscriptContent {...topics} />
+        ))}
+      </div>
     </div>
   )
 }
