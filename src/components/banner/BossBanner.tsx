9d3664c9b506021ddb5c82c0b7a13a6da2ac1c85
0tuedon <90271995+0tuedon@users.noreply.github.com>
2024-11-27 17:44:02 +0100

    fix:(version): version update and put bossBanner on homepage only

--- src/components/banner/BossBanner.tsx
+++ src/components/banner/BossBanner.tsx
@@ -1,10 +1,14 @@
 "use client";
 
 import { Banner } from "@bitcoin-dev-project/bdp-ui";
+import { usePathname } from "next/navigation";
+
 
 const BossBanner = () => {
+  const pathname = usePathname()
+  const isHome = pathname === "/"
   return (
-    <div className={`w-full bg-bdp-background sticky`}>
+    <div className={`w-full bg-bdp-background sticky top-[var(--header-height)] z-40 ${!isHome ? "hidden" : ""}`}>
       <Banner
         headingText="Start your career in bitcoin open source —"
         linkText="APPLY TODAY"
