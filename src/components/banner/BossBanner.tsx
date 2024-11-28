3877ce9bff6e964c18c83f445598d456e4eea97f
0tuedon <90271995+0tuedon@users.noreply.github.com>
2024-11-28 12:40:45 +0100

    Add boss banner (#63)
    * chore(banner): add boss banner
    
    * fix(version): bdp-ui version updated to 1.5.0
    
    * fix:(version): version update and put bossBanner on homepage only

---
+++ src/components/banner/BossBanner.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { Banner } from "@bitcoin-dev-project/bdp-ui";
+import { usePathname } from "next/navigation";
+
+
+const BossBanner = () => {
+  const pathname = usePathname()
+  const isHome = pathname === "/"
+  return (
+    <div className={`w-full bg-bdp-background sticky top-[var(--header-height)] z-40 ${!isHome ? "hidden" : ""}`}>
+      <Banner
+        headingText="Start your career in bitcoin open source —"
+        linkText="APPLY TODAY"
+        linkTo="https://learning.chaincode.com/#BOSS"
+        hasBoss
+      />
+    </div>
+  );
+};
+
+export default BossBanner;
