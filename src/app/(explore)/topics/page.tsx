5907312f0f7124dfb3f3ff98f1036699c45fd771
Andreas <kouloumosa@gmail.com>
2024-12-04 14:02:10 +0200

    fix(contentlayer): Improve Topics Processing and Generation (#61)
    * fix(contentlayer): topics counts generation
    
    - fix issues with generation of topics
    - streamline the code by grouping the calculations and generation into a single method
    - more meaningful names for json outputs
    
    * refactor(contentlayer): simplify topics processing and clarify terminology
    
    - terminology change to refer to "topics" instead of "categories"
    - Simplify data processing to do a single pass and use a single structure
    - Consolidate JSON generation into cleaner functions
    
    * refactor(contentlayer): use our own topics index
    
    - start using our own topics index instead of optech's
    - add different display option for misc topics in categories

--- src/app/(explore)/topics/page.tsx
+++ src/app/(explore)/topics/page.tsx
@@ -1,9 +1,8 @@
 import React from "react";
 import TranscriptContentPage from "@/components/explore/TranscriptContentPage";
-import allTopics from "@/public/topics-data.json";
+import allTopics from "@/public/topics-counts.json";
 
 const TopicsPage = () => {
-  
   return (
     <div className="flex flex-col  text-black">
         <TranscriptContentPage header="Topics" data={allTopics} description="Bitcoin is made up of an endless amount of topics, and there’s no shortage of rabbit holes to go down. "  type="alphabet" linkName="tags"/>
