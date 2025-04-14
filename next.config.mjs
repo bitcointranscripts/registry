70f0cff13d431252d3fd3bc10b9e72eafc439f5c
0tuedon <90271995+0tuedon@users.noreply.github.com>
2025-04-14 04:30:27 +0100

    fix: sources and status json (#91)

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,14 @@ const nextConfig = {
     return {
       fallback: [
         {
+          source: "/sources.json",
+          destination: "/gh-pages/sources.json",
+        },  
+        {
+          source: "/status.json",
+          destination: "/gh-pages/status.json",
+        },  
+        {
           source: "/:path((?!.*\\.[a-zA-Z0-9]{1,4}$).*)", // Matches paths without a valid file extension
           destination: "/transcript/:path*", // Rewrite to /transcripts/[path...]
         },
